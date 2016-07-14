cdef15314eff408e614ca656c3d5b225e7ae564a
sameh sayed <seniorsam@users.noreply.github.com>
2016-07-14 13:26:22 +0200

    Add files via upload

---
+++ assets/js/acc_app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$(".panel-trigger").click(function(e){
+		e.preventDefault();
+		// this panel trigger
+		var self = $(this);
+		// panel content wrapper
+		var next = self.next();
+		// rest of panel contents
+		var restOfContents = $(this).parents('#acc-panels-wrapper').find("div.acc-inner-content");
+		// open the content block that relate to the current trigger
+		if(next.css("display") == "none"){
+			restOfContents.slideUp('medium');
+			next.slideDown("medium");
+		} else {
+			restOfContents.slideUp('medium');
+			next.slideUp("medium");			
+		}
+		// close the rest of contents #337ab7 #ddd #f51414
+		console.log(restOfContents);
+	});
+});
